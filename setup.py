71f54e2dc3dcaee6dc2d06b426561fc371712504
Peter Wen <peter@whitehalmos.org>
2014-01-01 11:38:36 -0500

    0.3.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='rgkit',
-    version='0.3.0',
+    version='0.3.1',
     description='Robot Game Testing Kit',
     maintainer='Peter Wen',
     maintainer_email='peter@whitehalmos.org',
