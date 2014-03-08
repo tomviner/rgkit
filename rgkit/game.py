ec60f1024e10edb0a545cc1a0738b8cf361b8df6
berleon <github@leon-sixt.de>
2014-03-08 17:28:42 +0100

    Add runner class
    To train self learning robots, I want be able to include rgkit as a
    library.

--- rgkit/game.py
+++ rgkit/game.py
@@ -1,4 +1,5 @@
 import imp
+import os
 import random
 import sys
 import traceback
@@ -23,12 +24,13 @@ class NullDevice(object):
 class Player(object):
     def __init__(self, code=None, robot=None):
         self._player_id = None  # must be set using set_player_id
-
         if code is not None:
             self._code = code
+            self._name = os.path.splitext(os.path.basename(code))[0]
             self.reload()
         elif robot is not None:
             self._module = None
+            self._name = str(robot.__class__).split('.')[-1]
             self._robot = robot
         else:
             raise Exception('you need to provide code or a robot')
@@ -80,11 +82,14 @@ class Player(object):
 
         return actions
 
+    def name(self):
+        return self._name
+
 
 class Game(object):
-    def __init__(self, players, record_actions=False,
-                 record_history=False, print_info=False,
-                 seed=None, quiet=0, symmetric=False):
+    def __init__(self, players, record_actions=False, record_history=False,
+                 print_info=False, seed=None, quiet=0, delta_callback=None,
+                 symmetric=False):
         self._players = players
         for i, player in enumerate(self._players):
             player.set_player_id(i)
@@ -96,6 +101,7 @@ class Game(object):
         self.seed = str(seed)
         self._random = random.Random(self.seed)
         self._quiet = quiet
+        self._delta_callback = delta_callback
         self._state = GameState(use_start=True, seed=self.seed,
                                 symmetric=symmetric)
 
@@ -205,6 +211,8 @@ class Game(object):
         actions = self._get_robots_actions()
 
         delta = self._state.get_delta(actions)
+        if self._delta_callback is not None:
+            self._delta_callback(delta)
 
         if self._record_actions:
             actions_on_turn = self._calculate_actions_on_turn(delta, actions)
--- rgkit/run.py
+++ rgkit/run.py
@@ -3,24 +3,13 @@
 import argparse
 from argparse import RawTextHelpFormatter
 import ast
+import copy
 import imp
 import inspect
 import pkg_resources
 import random
-import sys
 import os
-
-_is_multiprocessing_supported = True
-try:
-    import multiprocessing
-except ImportError:
-    # the OS does not support it. See http://bugs.python.org/issue3770
-    _is_multiprocessing_supported = False
-
-if _is_multiprocessing_supported:
-    _rgcurses_lock = multiprocessing.Lock()
-else:
-    _rgcurses_lock = None
+import sys
 
 try:
     imp.find_module('rgkit')
@@ -31,54 +20,268 @@ except ImportError:
     parentdir = dirname(cdir)
     sys.path.insert(0, parentdir)
 
-import rgkit.settings
-from rgkit.settings import settings
+from rgkit.settings import settings as default_settings
+from rgkit.settings import init as settings_init
 from rgkit import game
+from rgkit.game import Player
 
 
-parser = argparse.ArgumentParser(description="Robot game execution script.",
-                                 formatter_class=RawTextHelpFormatter)
-parser.add_argument("player1",
-                    help="File containing first robot class definition.")
-parser.add_argument("player2",
-                    help="File containing second robot class definition.")
-parser.add_argument("-m", "--map",
-                    help="User-specified map file.",
-                    type=argparse.FileType('r'),
-                    default=pkg_resources.resource_filename('rgkit',
-                                                            'maps/default.py'))
-parser.add_argument("-c", "--count", type=int,
-                    default=1,
-                    help="Game count, default: 1, multithreading if >1")
-parser.add_argument("-A", "--animate", action="store_true",
-                    default=False,
-                    help="Enable animations in rendering.")
-parser.add_argument("-q", "--quiet", action="count",
-                    help="Quiet execution.\n\
--q : suppresses bot stdout\n\
--qq: suppresses bot stdout and stderr\n\
--qqq: supresses all rgkit and bot output")
-group = parser.add_mutually_exclusive_group()
-group.add_argument("-H", "--headless", action="store_true",
-                   default=False,
-                   help="Disable rendering game output.")
-group.add_argument("-T", "--play-in-thread", action="store_true",
-                   default=False,
-                   help="Separate GUI thread from robot move calculations.")
-group.add_argument("-C", "--curses", action="store_true",
-                   default=False,
-                   help="Display game in command line using curses.")
-parser.add_argument("--game-seed",
-                    default=random.randint(0, settings.max_seed),
-                    help="Appended with game countfor per-match seeds.")
-parser.add_argument("--match-seeds", nargs='*',
-                    help="Used for random seed of the first matches in order.")
-parser.add_argument("-s", "--symmetric", action="store_true",
-                    default=False,
-                    help="Bots spawn symmetrically.")
-parser.add_argument("-M", "--heatmap", action="store_true",
-                    default=False,
-                    help="Print heatmap after playing a number of games.")
+class Options:
+    def __init__(self, map_filepath=None, print_info=False,
+                 animate_render=False, play_in_thread=False, curses=False,
+                 game_seed=None, match_seeds=None, quiet=0, symmetric=False,
+                 n_of_games=1):
+
+        if map_filepath is None:
+            map_filepath = os.path.join(os.path.dirname(__file__),
+                                        'maps/default.py')
+
+        self.map_filepath = map_filepath
+        self.print_info = print_info
+        self.animate_render = animate_render
+        self.play_in_thread = play_in_thread
+        self.curses = curses
+        self.game_seed = game_seed
+        self.match_seeds = match_seeds
+        self.quiet = quiet
+        self.symmetric = symmetric
+        self.n_of_games = n_of_games
+
+    def __eq__(self, other):
+        return (self.map_filepath == other.map_filepath and
+                self.print_info == other.print_info and
+                self.animate_render == other.animate_render and
+                self.play_in_thread == other.play_in_thread and
+                self.curses == other.curses and
+                self.match_seeds == other.match_seeds and
+                self.quiet == other.quiet and
+                self.n_of_games == other.n_of_games and
+                self.symmetric == other.symmetric)
+
+
+class Runner:
+    def __init__(self, player1, player2, settings=None, options=None,
+                 delta_callback=None):
+
+        if settings is None:
+            settings = Runner.default_settings()
+        if options is None:
+            options = Options()
+
+        self._map_data = ast.literal_eval(open(options.map_filepath).read())
+        settings_init(self._map_data)
+        self.settings = settings
+        self._players = [player1, player2]
+        self._delta_callback = delta_callback
+        self._names = [player1.name(), player2.name()]
+        self.options = options
+
+        if Runner.is_multiprocessing_supported():
+            import multiprocessing
+            self._rgcurses_lock = multiprocessing.Lock()
+        else:
+            self._rgcurses_lock = None
+
+    @staticmethod
+    def from_robots(robot1, robot2, settings=None, options=None,
+                    delta_callback=None):
+
+        return Runner(Player(robot=robot1), Player(robot=robot2),
+                      settings=settings, options=options,
+                      delta_callback=delta_callback)
+
+    @staticmethod
+    def from_command_line_args(args):
+        map_name = os.path.join(args.map)
+
+        options = Options(map_filepath=map_name,
+                          print_info=not args.headless,
+                          n_of_games=args.count,
+                          animate_render=args.animate,
+                          play_in_thread=args.play_in_thread,
+                          quiet=args.quiet,
+                          curses=args.curses,
+                          match_seeds=args.match_seeds,
+                          symmetric=args.symmetric)
+        player1 = Runner._make_player(args.player1)
+        player2 = Runner._make_player(args.player2)
+        return Runner(player1, player2, options=options)
+
+    @staticmethod
+    def _make_player(file_name):
+        try:
+            with open(file_name) as f:
+                return game.Player(code=f.read())
+        except IOError, msg:
+            if pkg_resources.resource_exists('rgkit', file_name):
+                bot_filename = pkg_resources.resource_filename('rgkit',
+                                                               file_name)
+                with open(bot_filename) as f:
+                    return game.Player(code=f.read())
+            raise IOError(msg)
+
+    @staticmethod
+    def default_map():
+        map_path = os.path.join(os.path.dirname(__file__), 'maps/default.py')
+        return map_path
+
+    @staticmethod
+    def default_settings():
+        return default_settings
+
+    def game(self, record_turns=False, unit_testing=False):
+        return game.Game(self._players, record_turns=record_turns,
+                         unit_testing=unit_testing)
+
+    def run(self):
+        scores = []
+        print(self.options.n_of_games)
+        for i in xrange(self.options.n_of_games):
+            # A sequential, deterministic seed is used for each match that can
+            # be overridden by user provided ones.
+            match_seed = str(self.options.game_seed) + '-' + str(i)
+            if self.options.match_seeds and i < len(self.options.match_seeds):
+                match_seed = self.options.match_seeds[i]
+            result = self.play()
+            scores.append(result)
+            if self.options.quiet >= 3 and not self.options.print_info:
+                self.unmute_all()
+            print '{0} - seed: {1}'.format(result, match_seed)
+        return scores
+
+    def play(self):
+        if self.options.play_in_thread:
+            g = game.ThreadedGame(self._players,
+                                  print_info=self.options.print_info,
+                                  record_actions=self.options.print_info,
+                                  record_history=True,
+                                  seed=self.options.match_seeds,
+                                  quiet=self.options.quiet,
+                                  delta_callback=self._delta_callback,
+                                  symmetric=self.options.symmetric)
+        else:
+            g = game.Game(self._players,
+                          print_info=self.options.print_info,
+                          record_actions=self.options.print_info,
+                          record_history=True,
+                          seed=self.options.match_seeds,
+                          quiet=self.options.quiet,
+                          delta_callback=self._delta_callback,
+                          symmetric=self.options.symmetric)
+
+        if self.options.print_info and not self.options.curses:
+            # only import render if we need to render the game;
+            # this way, people who don't have tkinter can still
+            # run headless
+            from rgkit.render import render
+
+        g.run_all_turns()
+
+        if self.options.print_info and not self.options.curses:
+            #print "rendering %s animations" % ("with"
+            #                               if animate_render else "without")
+            render.Render(g, self.options.animate_render, names=self._names)
+
+        # TODO: Displaying multiple games using curses is still a little bit
+        # buggy but at least it doesn't completely screw up the state of the
+        # terminal anymore.  The plan is to show each game sequentially.
+        # Concurrency in run.py needs some more work before the bugs can be
+        # fixed. Need to make sure nothing is printing when curses is running.
+        if self.options.print_info and self.options.curses:
+            from rgkit import rgcurses
+            rgc = rgcurses.RGCurses(g, self._names)
+            if self._rgcurses_lock:
+                self.rgcurses_lock.acquire()
+            rgc.run()
+            if self._rgcurses_lock:
+                self._rgcurses_lock.release()
+
+        return g.get_scores()
+
+    @staticmethod
+    def is_multiprocessing_supported():
+        is_multiprocessing_supported = True
+        try:
+            imp.find_module('multiprocessing')
+        except ImportError:
+            # the OS does not support it. See http://bugs.python.org/issue3770
+            is_multiprocessing_supported = False
+
+        return is_multiprocessing_supported
+
+
+def _task(arg):
+    return Runner.from_command_line_args(arg).run()
+
+
+def run_concurrently(args):
+    import multiprocessing
+    num_cpu = multiprocessing.cpu_count()
+    (games_per_cpu, remainder) = divmod(args.count, num_cpu)
+    data = []
+    for i in xrange(num_cpu):
+        copy_args = copy.deepcopy(args)
+
+        if i == 0:
+            copy_args.count = games_per_cpu + remainder
+        else:
+            copy_args.count = games_per_cpu
+
+        data.append(copy_args)
+
+    pool = multiprocessing.Pool(num_cpu)
+    results = pool.map(_task, data, 1)
+    return [score for scores in results for score in scores]
+
+
+def arg_parser():
+    parser = argparse.ArgumentParser(description=
+                                     "Robot game execution script.",
+                                     formatter_class=RawTextHelpFormatter)
+    parser.add_argument("player1",
+                        help="File containing first robot class definition.")
+    parser.add_argument("player2",
+                        help="File containing second robot class definition.")
+    defalut_map = pkg_resources.resource_filename('rgkit', 'maps/default.py')
+    parser.add_argument("-m", "--map",
+                        help="User-specified map file.",
+                        default=defalut_map)
+    parser.add_argument("-c", "--count", type=int,
+                        default=1,
+                        help="Game count, default: 1, multithreading if >1")
+    parser.add_argument("-A", "--animate", action="store_true",
+                        default=False,
+                        help="Enable animations in rendering.")
+    parser.add_argument("-q", "--quiet", action="count",
+                        help="Quiet execution.\n\
+    -q : suppresses bot stdout\n\
+    -qq: suppresses bot stdout and stderr\n\
+    -qqq: supresses all rgkit and bot output")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-H", "--headless", action="store_true",
+                       default=False,
+                       help="Disable rendering game output.")
+    group.add_argument("-T", "--play-in-thread", action="store_true",
+                       default=False,
+                       help="Separate GUI thread from robot move calculations."
+                       )
+    group.add_argument("-C", "--curses", action="store_true",
+                       default=False,
+                       help="Display game in command line using curses.")
+    parser.add_argument("--game-seed",
+                        default=random.randint(0, default_settings.max_seed),
+                        help="Appended with game countfor per-match seeds.")
+    parser.add_argument("--match-seeds", nargs='*',
+                        help="Used for random seed of the first matches"
+                        + " in order.")
+    parser.add_argument("-s", "--symmetric", action="store_true",
+                        default=False,
+                        help="Bots spawn symmetrically.")
+    parser.add_argument("-M", "--heatmap", action="store_true",
+                        default=False,
+                        help="Print heatmap after playing a number of games.")
+
+    return parser
 
 
 def mute_all():
@@ -89,155 +292,6 @@ def mute_all():
 def unmute_all():
     sys.stdout = sys.__stdout__
     sys.stderr = sys.__stderr__
-
-
-def make_player(fname):
-    try:
-        with open(fname) as f:
-            return game.Player(code=f.read())
-    except IOError, msg:
-        if pkg_resources.resource_exists('rgkit', fname):
-            with open(pkg_resources.resource_filename('rgkit', fname)) as f:
-                return game.Player(code=f.read())
-        raise IOError(msg)
-
-
-def play(players, print_info=True, animate_render=False, play_in_thread=False,
-         curses=False, match_seed=None, names=["Red", "Blue"], quiet=0,
-         symmetric=False):
-    if play_in_thread:
-        g = game.ThreadedGame(players,
-                              print_info=print_info,
-                              record_actions=print_info,
-                              record_history=True,
-                              seed=match_seed,
-                              quiet=quiet,
-                              symmetric=symmetric)
-    else:
-        g = game.Game(players,
-                      print_info=print_info,
-                      record_actions=print_info,
-                      record_history=True,
-                      seed=match_seed,
-                      quiet=quiet,
-                      symmetric=symmetric)
-
-    if print_info and not curses:
-        # only import render if we need to render the game;
-        # this way, people who don't have tkinter can still
-        # run headless
-        from rgkit.render import render
-
-    g.run_all_turns()
-
-    if print_info and not curses:
-        #print "rendering %s animations" % ("with"
-        #                                   if animate_render else "without")
-        render.Render(g, animate_render, names=names)
-
-    # TODO:
-    # Displaying multiple games using curses is still a little bit buggy but at
-    # least it doesn't completely screw up the state of the terminal anymore.
-    # The plan is to show each game sequentially. Concurrency in run.py needs
-    # some more work before the bugs can be fixed. Need to make sure nothing
-    # is printing when curses is running.
-    if print_info and curses:
-        from rgkit import rgcurses
-        rgc = rgcurses.RGCurses(g, names)
-        if _rgcurses_lock:
-            _rgcurses_lock.acquire()
-        rgc.run()
-        if _rgcurses_lock:
-            _rgcurses_lock.release()
-
-    return g.get_scores()
-
-
-def test_runs_sequentially(args):
-    players = [make_player(args.player1), make_player(args.player2)]
-    names = [bot_name(args.player1), bot_name(args.player2)]
-    scores = []
-    for i in xrange(args.count):
-        # A sequential, deterministic seed is used for each match that can be
-        # overridden by user provided ones.
-        match_seed = str(args.game_seed) + '-' + str(i)
-        if args.match_seeds and i < len(args.match_seeds):
-            match_seed = args.match_seeds[i]
-        result = play(players,
-                      not args.headless,
-                      args.animate,
-                      args.play_in_thread,
-                      args.curses,
-                      match_seed=match_seed,
-                      names=names,
-                      quiet=args.quiet,
-                      symmetric=args.symmetric)
-        scores.append(result)
-        if args.quiet >= 3 and args.headless:
-            unmute_all()
-        print '{0} - seed: {1}'.format(result, match_seed)
-    return scores
-
-
-def task(data):
-    (player1,
-     player2,
-     headless,
-     animate,
-     play_in_thread,
-     curses,
-     match_seed,
-     quiet,
-     symmetric) = data
-
-    result = play(
-        [
-            make_player(player1),
-            make_player(player2)
-        ],
-        not headless,
-        animate,
-        play_in_thread,
-        curses,
-        match_seed=match_seed,
-        names=[
-            bot_name(player1),
-            bot_name(player2)
-        ],
-        quiet=quiet,
-        symmetric=symmetric
-    )
-    if quiet >= 3 and headless:
-        unmute_all()
-    print '{0} - seed: {1}'.format(result, match_seed)
-    return result
-
-
-def test_runs_concurrently(args):
-    data = []
-    for i in xrange(args.count):
-        match_seed = str(args.game_seed) + '-' + str(i)
-        if args.match_seeds and i < len(args.match_seeds):
-            match_seed = args.match_seeds[i]
-        data.append([
-            args.player1,
-            args.player2,
-            args.headless,
-            args.animate,
-            args.play_in_thread,
-            args.curses,
-            match_seed,
-            args.quiet,
-            args.symmetric
-        ])
-    num_cpu = multiprocessing.cpu_count() - 1
-    if num_cpu == 0:
-        num_cpu = 1
-    return multiprocessing.Pool(num_cpu).map(task, data)
-
-
-def bot_name(path_to_bot):
-    return os.path.splitext(os.path.basename(path_to_bot))[0]
 
 
 def print_score_grid(scores, player1, player2, size):
@@ -289,17 +343,16 @@ def print_score_grid(scores, player1, player2, size):
 
 
 def main():
-    args = parser.parse_args()
+    args = arg_parser().parse_args()
     if args.quiet >= 3:
         mute_all()
 
-    map_data = ast.literal_eval(args.map.read())
-    rgkit.settings.init(map_data)
     print('Game seed: {0}'.format(args.game_seed))
 
-    runner = test_runs_sequentially
-    if _is_multiprocessing_supported and args.count > 1:
-        runner = test_runs_concurrently
+    if Runner.is_multiprocessing_supported() and args.count > 1:
+        runner = run_concurrently
+    else:
+        runner = lambda _args: Runner.from_command_line_args(_args).run()
     scores = runner(args)
 
     if args.count > 1:
