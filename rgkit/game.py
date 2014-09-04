1eb13969871f1f24f84193e82fe1a37534301301
Antal Szabó <szabo.antal.92@gmail.com>
2014-09-04 23:57:04 +0200

    Convert returned coordinates to int

--- rgkit/game.py
+++ rgkit/game.py
@@ -72,6 +72,9 @@ class Player(object):
                     'Bot {0}: {1} is not a valid action from {2}'.format(
                         robot.robot_id + 1, action, robot.location)
                 )
+            
+            if action[0] in ['move', 'attack']:
+                action[1] = (int(action[1][0]), int(action[1][1]))
 
         except:
             traceback.print_exc(file=sys.stdout)
