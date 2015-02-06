d35a4b0061ca29ff5e391497e82b8f03aecb8e5e
Peter Wen <peter@whitehalmos.org>
2015-02-06 17:46:35 -0500

    Fix tests, use flake8 instead of pyflakes.

--- test/acceptance_test.py
+++ test/acceptance_test.py
@@ -1,6 +1,10 @@
 import unittest
 from os import path
-import subprocess
+# TODO: Make this hack better -_-.
+try:
+    import commands as runcommand
+except:
+    import subprocess as runcommand
 
 
 class TestAttack(unittest.TestCase):
@@ -9,12 +13,12 @@ class TestAttack(unittest.TestCase):
 
         # test if -h runs
         cmd = testdir + "/../rgkit/run.py -h"
-        exit_code, ignore = subprocess.getstatusoutput(cmd)
+        exit_code, ignore = runcommand.getstatusoutput(cmd)
         self.assertEqual(exit_code, 0)
 
         robot_file = testdir + "/acceptance_robot.py"
         # test simple robot
         cmd = testdir + "/../rgkit/run.py -H {0} {0}".format(robot_file)
 
-        exit_code, out = subprocess.getstatusoutput(cmd)
+        exit_code, out = runcommand.getstatusoutput(cmd)
         self.assertEqual(exit_code, 0)
