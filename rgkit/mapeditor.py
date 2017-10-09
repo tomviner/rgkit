d6ef6b65e3ca676d578309b16471a787db921677
Tom Viner <tom@viner.tv>
2017-10-09 19:38:48 +0100

    import tkinter for py3

--- rgkit/mapeditor.py
+++ rgkit/mapeditor.py
@@ -3,7 +3,10 @@ from __future__ import division
 from __future__ import print_function
 
 import ast
-import Tkinter
+try:
+    import Tkinter
+except ImportError:
+    import tkinter as Tkinter
 import sys
 import os.path
 
