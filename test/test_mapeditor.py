b7f7612fd82fb8c6ba2a7a50c0187d1575a8ccbc
Tom Viner <tom@viner.tv>
2017-10-09 22:10:53 +0100

    improve new tests

--- test/test_mapeditor.py
+++ test/test_mapeditor.py
@@ -1,4 +1,10 @@
-def test_mapeditor_importable():
+import sys
+
+def test_mapeditor_importable(monkeypatch, capsys):
     """At least catch import errors!"""
     from rgkit.mapeditor import main
+    # empty command line args
+    monkeypatch.setattr(sys, 'argv', sys.argv[:1])
     main()
+    out, err = capsys.readouterr()
+    assert 'usage: ' in out
--- test/test_utils.py
+++ test/test_utils.py
@@ -2,6 +2,8 @@ from rgkit.render.utils import rgb_to_hex
 
 
 def test_rgb_to_hex():
-    assert rgb_to_hex(1, 0.5, 0.25) == '#ff7f3f'
+    expected_hex = '#ff7f3f'
+
+    assert rgb_to_hex(1, 0.5, 0.25) == expected_hex
     assert rgb_to_hex(
-        255, 127, 63, normalized=False) == '#ff7f3f'
+        255, 127, 63, normalized=False) == expected_hex
