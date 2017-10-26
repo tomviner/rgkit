462746739e0c571d7d275a77412928367d0f3f5b
Madda <nobody.youknow@local>
2017-10-26 20:20:39 +0100

    Introduce bugs

--- rgkit/bots/team0xf/campbot.py
+++ rgkit/bots/team0xf/campbot.py
@@ -11,7 +11,8 @@ class Robot:
         if my_friendly_neighbours:
             return ['move', rg.toward(self.location, rg.CENTER_POINT)]
 
-        bad_neighbours = self.get_neighbours(game, friendly=False)
+        bad_neighbours = self.get_neighbours(game, friendly=False,
+                                             onlyspawn=False)
         # CAMP THE SPAWN! SHOOT NOW!
         shoot = 'attack'
         if bad_neighbours:
@@ -19,16 +20,21 @@ class Robot:
                 print('HAHAHA! DIE DIE DIE!')
             return [shoot, bad_neighbours[0]['location']]
 
+
+
         return ['guard']
 
-    def get_neighbours(self, game, friendly=True):
+    def get_neighbours(self, game, friendly=True, onlyspawn=True):
         if friendly:
             player_id = self.player_id
         else:
             player_id = (self.player_id + 1) % 2
-        return [
+        return  [
             bot for bot in game.robots.values()
             if bot['player_id'] == player_id
-            and 'spawn' in rg.loc_types(bot.location)
+            and (
+                not onlyspawn or
+                'spawn' in rg.loc_types(bot.location)
+            )
             and bot.location in rg.locs_around(self.location)
         ]
