2d2b45d13a185b2ab9525f2547a1c0143d60d2cd
Daniel Pope <mauve@mauveweb.co.uk>
2017-10-26 20:35:02 +0100

    Fix flake8

--- rgkit/bots/team_11.py
+++ rgkit/bots/team_11.py
@@ -5,8 +5,13 @@ import random
 class Robot:
     def act(self, game):
         center = rg.CENTER_POINT
-        enemy_bots = [b for b in game.robots.values() if b.player_id != self.player_id]
-        target = min(enemy_bots, key=lambda bot: rg.wdist(bot.location, center))
+        enemy_bots = [
+            b for b in game.robots.values()
+            if b.player_id != self.player_id]
+        target = min(
+            enemy_bots,
+            key=lambda bot: rg.wdist(bot.location, center)
+        )
 
         adjacent_bots = []
         for enemy in enemy_bots:
@@ -17,9 +22,15 @@ class Robot:
         elif adjacent_bots:
             return ['attack', min(adjacent_bots, key=lambda b: b.hp).location]
 
-        adj = rg.locs_around(target.location, filter_out=('invalid', 'obstacle'))
+        adj = rg.locs_around(
+            target.location,
+            filter_out=('invalid', 'obstacle')
+        )
         closest_locs = min(rg.wdist(loc, self.location) for loc in adj)
-        dests = [loc for loc in adj if rg.wdist(loc, self.location) == closest_locs]
+        dests = [
+            loc
+            for loc in adj
+            if rg.wdist(loc, self.location) == closest_locs]
         dest = random.choice(dests)
         if rg.wdist(target.location, self.location) == 1:
             return ['attack', target.location]
