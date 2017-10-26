d77afb396ca88357d629dbac5e7a683adc27d9ac
Madda <nobody.youknow@local>
2017-10-26 20:06:41 +0100

    Be nice, camp spawn points except friendlies

--- rgkit/bots/team0xf/campbot.py
+++ rgkit/bots/team0xf/campbot.py
@@ -6,4 +6,11 @@ class Robot:
                   if bot['player_id'] == self.player_id]
         if 'spawn' in rg.loc_types(self.location):
             return ['move', rg.toward(self.location,rg.CENTER_POINT)]
+        
+        my_friendly_neighbours = [ bot for bot in game.robots.values()
+            if bot['player_id'] == self.player_id
+                and 'spawn' in rg.loc_types(bot.location)
+                and bot.location in rg.locs_around(self.location)]
+        if my_friendly_neighbours:
+            return ['move', rg.toward(self.location,rg.CENTER_POINT)]
         return ['guard']
