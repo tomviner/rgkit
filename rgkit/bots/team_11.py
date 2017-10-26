ad6ab1c64720cf644448006d6de3158c4fd33538
Daniel Pope <mauve@mauveweb.co.uk>
2017-10-26 20:26:00 +0100

    Add suicide and attack weakest adjacent bot

--- rgkit/bots/team_11.py
+++ rgkit/bots/team_11.py
@@ -7,9 +7,16 @@ class Robot:
         center = rg.CENTER_POINT
         enemy_bots = [b for b in game.robots.values() if b.player_id != self.player_id]
         target = min(enemy_bots, key=lambda bot: rg.wdist(bot.location, center))
+
+        adjacent_bots = []
         for enemy in enemy_bots:
             if rg.wdist(enemy.location, self.location) == 1:
-                return ['attack', enemy.location]
+                adjacent_bots.append(enemy)
+        if len(adjacent_bots) > self.hp / 9:
+            return ['suicide']
+        elif adjacent_bots:
+            return ['attack', min(adjacent_bots, key=lambda b: b.hp).location]
+
         adj = rg.locs_around(target.location, filter_out=('invalid', 'obstacle'))
         dest = min(adj, key=lambda loc: rg.wdist(loc, self.location))
         if rg.wdist(target.location, self.location) > 1:
